2f62583e54ae7fad35322a66ce080863951eda51
Jason Hall <imjasonh@gmail.com>
2013-12-21 23:57:09 -0500

    Initial commit of Go files

---
+++ pointer.go
@@ -0,0 +1,151 @@
+package pointer
+
+import (
+	"fmt"
+	"html/template"
+	"math/rand"
+	"net/http"
+	"strings"
+
+	"appengine"
+	"appengine/channel"
+	"appengine/memcache"
+)
+
+func init() {
+	http.HandleFunc("/", main)
+	http.HandleFunc("/update", update)
+	http.HandleFunc("/leave", leave)
+}
+
+func main(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	ids := getIDs(c)
+	id := fmt.Sprintf("%d", rand.Int())
+	found := false
+	for _, mid := range ids {
+		if id == mid {
+			found = true
+		}
+	}
+	if !found {
+		ids = append(ids, id)
+		_ = memcache.Set(c, &memcache.Item{
+			Key:   "ids",
+			Value: []byte(strings.Join(ids, ",")),
+		})
+	}
+
+	tok, _ := channel.Create(c, id)
+	tmpl.Execute(w, struct{ Token, ClientID string }{tok, id})
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	ids := getIDs(c)
+	x := r.FormValue("x")
+	y := r.FormValue("y")
+	from := r.FormValue("id")
+	for _, id := range ids {
+		if id != from {
+			_ = channel.Send(c, id, fmt.Sprintf("{\"x\":%s,\"y\":%s,\"id\":\"%s\"}", x, y, from))
+		}
+	}
+}
+
+func leave(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	ids := getIDs(c)
+	i := -1
+	id := r.FormValue("id")
+	for ni, mid := range ids {
+		if id == mid {
+			i = ni
+			break
+		}
+	}
+	if i != -1 {
+		ids = append(ids[:i], ids[i+1:]...)
+		_ = memcache.Set(c, &memcache.Item{
+			Key:   "ids",
+			Value: []byte(strings.Join(ids, ",")),
+		})
+	}
+	for _, id := range ids {
+		_ = channel.Send(c, id, fmt.Sprintf("{\"x\":-1,\"y\":-1,\"id\":\"%s\"}", id))
+	}
+}
+
+func getIDs(c appengine.Context) []string {
+	var ids []string
+	if i, err := memcache.Get(c, "ids"); err == nil {
+		ids = strings.Split(string(i.Value), ",")
+	}
+	return ids
+}
+
+var tmpl = template.Must(template.New("n").Parse(`
+<!doctype html>
+<html style="height:100%;">
+  <head><title>Pointer Party!</title></head>
+  <body style="height:100%;font-family:Arial;font-size:10px;">
+    <script src='/_ah/channel/jsapi'></script>
+    <script type="text/javascript">
+      var my_id = '{{.ClientID }}';
+      var checkEvery = 200; //ms
+      var body = document.getElementsByTagName('body')[0];
+
+      var channel = new goog.appengine.Channel('{{.Token}}');
+      var socket = channel.open();
+
+      socket.onmessage = function(response) {
+        var data = JSON.parse(response.data);
+        var x = data.x;
+        var y = data.y;
+        var id = data.id;
+        // Update the correct pointer's location, or show a new pointer
+        // console.log('receiving: x=' + x + ' y=' + y + ' id=' + id)
+        var img = document.getElementById(id);
+        if (img == null) {
+          img = new Image();
+          img.id = id;
+          img.src = '/pointer.png';
+          img.style.position = 'absolute';
+          body.appendChild(img);
+        }
+        if (x < 0 || y < 0) {
+          img.style.display = 'none';
+          img = null;
+        } else {
+                  img.style.left = x + 'px';
+          img.style.top = y + 'px';
+        }
+      }
+
+      var actualonmousemove = function(ev) {
+        body.onmousemove = null;
+        setTimeout('body.onmousemove = actualonmousemove', checkEvery);
+
+        // Post the current mouse position to the server.
+        var xhr = new XMLHttpRequest();
+        var x = ev.clientX;
+        var y = ev.clientY;
+        var path = '/update?x=' + x + '&y=' + y + '&id=' + my_id;
+        // console.log('sending: x=' + x + ' y=' + y);
+        xhr.open('POST', path, true);
+        xhr.send();
+      }
+      body.onmousemove = actualonmousemove;
+
+      window.onbeforeunload = function() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/leave?id=' + my_id);
+        xhr.send();
+      }
+    </script>
+    <p><b>Welcome to Pointer Party!</b></p>
+    <p><b>Directions:</b> Move your mouse pointer. See the mouse pointers of other users on this page.</p>
+    <p>Built using <a href="http://code.google.com/appengine/docs/python/channel/overview.html">App Engine Channel API</a> (<a href="http://code.google.com/p/pointerparty/source/browse/">src</a>)
+  </body>
+</html>
+`))
